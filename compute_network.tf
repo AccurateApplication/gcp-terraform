7ebff50eb1ab2dde421971f50abfbec237eb6601
AccurateApplication <lucas@cederqvist.dev>
2021-05-25 18:58:50 +0200

    Create external IP, firewall rule (in VPC)

--- compute_network.tf
+++ compute_network.tf
@@ -1,13 +1,38 @@
-resource "google_compute_network" "vpc_network" {
-  name    = "star-vpc"
-  project = var.project_id
-  # region = var.region
+resource "google_compute_network" "default" {
+  name                    = "star-vpc"
+  project                 = var.project_id
   auto_create_subnetworks = false
 }
 
+resource "google_compute_firewall" "default" {
+  name        = "firewall"
+  description = "Used for wireguard"
+  network     = google_compute_network.default.name
+
+  allow {
+    protocol = "udp"
+    ports    = ["51820"]
+  }
+  source_ranges = ["0.0.0.0/0"] # Default?
+  source_tags   = ["wireguard"]
+
+}
+
 resource "google_compute_subnetwork" "custom-subnet" {
-  name          = google_compute_network.vpc_network.name
+  name          = google_compute_network.default.name
   region        = var.region
   ip_cidr_range = var.vpc_subnet_pub
-  network       = google_compute_network.vpc_network.id
+  network       = google_compute_network.default.id
+}
+
+resource "google_compute_address" "external" {
+  name         = "my-external-address"
+  address_type = "EXTERNAL"
+  region       = var.region
+  network_tier = "STANDARD"
+}
+
+
+output "external_address" {
+  value = google_compute_address.external.address
 }
--- variables.tf
+++ variables.tf
@@ -1,33 +1,34 @@
 variable "gcp_auth_file" {
-  type = string
   description = "Authentication file"
-  sensitive = true
+  # sensitive   = true # is the file contents or filepath considered sensitive? I guess path
+  type = string
 
 }
 
 variable "project_id" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 variable "region" {
-  type = string
   default = "europe-west1"
+  type    = string
 
 }
 
 variable "zone" {
-  type = string
   default = "europe-west1-b"
+  type    = string
 
 }
 
 variable "project_name" {
   description = "My first GCP project"
-  default = " Lucas First Project "
+  default     = " Lucas First Project "
   type        = string
 }
 
 variable "vpc_subnet_pub" {
+  type    = string
   default = "10.132.0.0/20"
-  type = string
 }
