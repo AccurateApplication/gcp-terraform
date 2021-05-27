d3f9c8bff55b5a779edc514d83b1278ff51dbdc0
AccurateApplication <lucas@cederqvist.dev>
2021-05-27 18:58:17 +0200

    Create inventory of all servers

--- output.tf
+++ output.tf
@@ -2,8 +2,12 @@ resource "local_file" "ansible_inventory" {
   content = templatefile("./templates/inventory.tpl",
     {
       ec2-ip   = google_compute_address.external.address,
-      ec2-desc = google_compute_instance.default.description
+      ec2-desc = google_compute_instance.bastion.description,
 
+      ec2-instances             = google_compute_instance.server[*], # Loop over
+      ec2-instances-name        = concat(google_compute_instance.server.*.name),
+      ec2-instances-description = concat(google_compute_instance.server.*.description),
+      ec2-instances-ip          = concat(google_compute_instance.server[*].network_interface[0].network_ip)
     }
 
   )
--- templates/inventory.tpl
+++ templates/inventory.tpl
@@ -1,2 +1,8 @@
-[servers]
+[bastion]
 ${ec2-ip} # ${ec2-desc}
+
+[servers]
+%{ for i, val in ec2-instances ~}
+${ec2-instances-name[i]} ansible_host=${ec2-instances-ip[i]} # ${ec2-instances-description[i]} #
+%{ endfor ~}
+
