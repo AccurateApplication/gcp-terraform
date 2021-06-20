2fc89f32256dc97f60eb267367e9844209b20b7a
AccurateApplication <lucas@cederqvist.dev>
2021-06-20 14:21:20 +0200

    Improve wg-key script

--- scripts/generate_wg_keys.sh
+++ scripts/generate_wg_keys.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 set -e
 umask 077
+secretDir="$(git rev-parse --show-toplevel)/secrets"
+if [[ ! -d  "$secretDir" ]]
+then
+    mkdir -p "$secretDir"
+fi
+
 # TODO place these in correct dir
-wg genkey > ""$PROJECT_BASE_DIR/secrets/privatekey""
-wg pubkey < ""$PROJECT_BASE_DIR/secrets/privatekey"" > ""$PROJECT_BASE_DIR/ansible/publickey""
+wg genkey > "$secretDir/privatekey"
+wg pubkey < "$secretDir"/privatekey > "$secretDir"/publickey
+
 
 # encrypt with vault
-
 echo -e "Will generate encrypted strings of publickey "
-ansible-vault encrypt_string "$(cat ""$PROJECT_BASE_DIR/secrets/publickey"")"
+ansible-vault encrypt_string "$(cat "$secretDir/publickey")"
 echo -e "^Paste into var file^\n\n"
 echo -e "Will generate encrypted strings of privatekey"
-ansible-vault encrypt_string "$(cat ""$PROJECT_BASE_DIR/secrets/publickey"")"
+ansible-vault encrypt_string "$(cat "$secretDir/privatekey")"
 echo -e "^Paste into var file^"
