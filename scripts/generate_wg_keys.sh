798f2431752ffa6124446c79d1db7c95f4224123
AccurateApplication <lucas@cederqvist.dev>
2021-06-20 13:32:19 +0200

    Generate ansible vault string of pub/priv key

--- scripts/generate_wg_keys.sh
+++ scripts/generate_wg_keys.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -e
 umask 077
-wg genkey > ""$PROJECT_BASE_DIR/ansible/privatekey""
-wg pubkey < ""$PROJECT_BASE_DIR/ansible/privatekey"" > ""$PROJECT_BASE_DIR/ansible/publickey""
+# TODO place these in correct dir
+wg genkey > ""$PROJECT_BASE_DIR/secrets/privatekey""
+wg pubkey < ""$PROJECT_BASE_DIR/secrets/privatekey"" > ""$PROJECT_BASE_DIR/ansible/publickey""
+
+# encrypt with vault
+
+echo -e "Will generate encrypted strings of publickey "
+ansible-vault encrypt_string "$(cat ""$PROJECT_BASE_DIR/secrets/publickey"")"
+echo -e "^Paste into var file^\n\n"
+echo -e "Will generate encrypted strings of privatekey"
+ansible-vault encrypt_string "$(cat ""$PROJECT_BASE_DIR/secrets/publickey"")"
+echo -e "^Paste into var file^"
